c41520949c890566942faad154724d2f61990107
Mark <martin_gal@mail.ru>
2023-04-04 15:00:00 +0300

    fix container element selector

--- js/miniatures.js
+++ js/miniatures.js
@@ -1,23 +1,30 @@
-const miniatureTemplate = document.querySelector('#picture').content;//Нашли template в документе и получили содержимое, фрагмент
-const container = miniatureTemplate.querySelector('.picture');//Нашли нужный шаблон
+//Нашли template в документе и получили содержимое, фрагмент
+const miniatureTemplate = document.querySelector('#picture').content;
+//Нашли нужный шаблон
+const container = document.querySelector('.pictures');
 
 const createMiniature = function ({comments, description, likes, url}) {
   const miniature = miniatureTemplate.cloneNode(true);
 
-  miniature.querySelector('.picture__img').src = url;//Адрес изображения url подставьте как атрибут src изображения
+  //Адрес изображения url подставьте как атрибут src изображения
+  miniature.querySelector('.picture__img').src = url;
   miniature.querySelector('.picture__img').alt = description;
-  miniature.querySelector('.picture__likes').textcontent = likes;//Количество лайков likes выведите в блок .picture__likes
-  miniature.querySelector('.picture__comments').textcontent = comments.length;//Количество комментариев comments выведите в блок .picture__comments
+  //Количество лайков likes выведите в блок .picture__likes
+  miniature.querySelector('.picture__likes').textcontent = likes;
+  //Количество комментариев comments выведите в блок .picture__comments
+  miniature.querySelector('.picture__comments').textcontent = comments.length;
 
   return miniature;
 };
 
-const drawMiniatures = function (pictures) {
-  const fragment = document.createDocumentFragment();//Создает новую пустую область DocumentFragment, в которую можно добавлять узлы DOM для построения внеэкранного дерева DOM.
-  pictures.forEach((picture) => {
-    const mini = createMiniature(picture);
+const drawMiniatures = function (picture) {
+  //Создает новую пустую область DocumentFragment, в которую можно добавлять узлы DOM для построения внеэкранного дерева DOM.
+  const fragment = document.createDocumentFragment();
+  //Метод forEach() выполняет указанную функцию один раз для каждого элемента в массиве.
+  picture.forEach((pictures) => {
+    const mini = createMiniature(pictures);
     fragment.append(mini);
-  });//Метод forEach() выполняет указанную функцию один раз для каждого элемента в массиве.
+  });
 
   container.append(fragment);
 };
--- js/util.js
+++ js/util.js
@@ -1,10 +1,12 @@
 
+/*Со стр. 4 по стр.22 Функция-генератор для получения случайных идентификаторов из указанного диапазона, и так, чтобы они не повторялись, пока не будут перебраны все числа из этого промежутка.*/
+//Функция по получению случайного целого числа в заданном интервале, включительно
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
-};/*Функция по получению случайного целого числа в заданном интервале, включительно*/
+};
 
 const createRandomIdFromRangeGenerator = (min, max) => {
   const previousValues = [];
@@ -17,8 +19,9 @@ const createRandomIdFromRangeGenerator = (min, max) => {
     previousValues.push(currentValue);
     return currentValue;
   };
-};/*Со стр. 1 по стр.18 Функция-генератор для получения случайных идентификаторов из указанного диапазона, и так, чтобы они не повторялись, пока не будут перебраны все числа из этого промежутка.*/
+};
 
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];/*Функция по поиску случайного элемента в переданном массиве*/
+//Функция по поиску случайного элемента в переданном массиве
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 export {getRandomInteger, createRandomIdFromRangeGenerator, getRandomArrayElement};
